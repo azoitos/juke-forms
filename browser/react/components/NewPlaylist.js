a0af19c6bace9fee4e12b6cad7d930bd446dc3f6
Alexander Zoitos <azoitos1@gmail.com>
2017-07-07 11:19:01 -0400

    Finished Part 3 - New Playlist

--- browser/react/components/NewPlaylist.js
+++ browser/react/components/NewPlaylist.js
@@ -1,7 +1,8 @@
 /**
  * Created by kieranderfus on 7/6/17.
  */
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class NewPlaylist extends Component {
 
@@ -10,43 +11,56 @@ export default class NewPlaylist extends Component {
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.state = {
-            inputValue: ''
+            inputValue: '',
+            edited: false
         };
     }
-
-    handleChange (event) {
-        this.setState({ inputValue: event.target.value })
+    
+    handleChange(event) {
+        this.setState({ inputValue: event.target.value, edited: true })
     }
-    handleSubmit (event) {
-        // this.setState({ inputValue: event.target.value })
+    handleSubmit(event) {
         event.preventDefault();
-        console.log(this.state.inputValue);
-        this.setState({inputValue: ''})
+
+        const inputValue = this.state.inputValue;
+        axios.post('/api/playlists', {name: inputValue})
+        .then(res => res.data)
+        .then(result => {
+            console.log(result)
+        })
+
+        this.setState({ inputValue: '', edited: false })
     }
 
     render() {
         return (
             <div className="well">
-                <form className="form-horizontal" onSubmit={ this.handleSubmit }>
+                <form className="form-horizontal" onSubmit={this.handleSubmit}>
                     <fieldset>
                         <legend>New Playlist</legend>
                         <div className="form-group">
                             <label className="col-xs-2 control-label">Name</label>
                             <div className="col-xs-10">
                                 <input className="form-control" type="text"
-                                       onChange = { this.handleChange }
-                                       value={this.state.inputValue}
+                                    onChange={this.handleChange}
+                                    value={this.state.inputValue}
                                 />
+                                {this.state.edited && !this.state.inputValue ? <div className="alert alert-warning">Please enter a name</div> : false}
+                                {this.state.edited && this.state.inputValue.length > 16 ? <div className="alert alert-warning">Name is too long</div> : false}
                             </div>
                         </div>
                         <div className="form-group">
                             <div className="col-xs-10 col-xs-offset-2">
-                                <button type="submit" className="btn btn-success">Create Playlist</button>
+                                <button type="submit" className="btn btn-success"
+                                    disabled={
+                                        !this.state.inputValue || this.state.inputValue.length > 16
+                                    }>Create Playlist
+                                </button>
                             </div>
                         </div>
                     </fieldset>
                 </form>
             </div>
         )
-    };
+    }
 }
